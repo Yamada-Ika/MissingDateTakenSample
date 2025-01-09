d3de1cffcebc1549d1e259bf42de2fde682ecc5c
Yamada_Ika <73375669+Yamada-Ika@users.noreply.github.com>
2025-01-09 17:11:34 +0900

    upgrade compileSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.yamadaika.missingdatetakensample"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.yamadaika.missingdatetakensample"
